c9d200dcac68710854aed934ff94533fb9770303
beatriz saito <beatriz.saito@play55.com.br>
2026-06-12 12:09:40 -0400

    feat: troca de jogador + declaracao de ganhador OK > estilizacao

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick} id={`square-${value}`}>{value}</button>;
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   return (
@@ -40,12 +39,48 @@ function Board() {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default function layoutGame() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handlePlay(nextSquares) {
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Ganhador: " + winner;
+  } else {
+    status = "Próximo jogador: " + (xIsNext ? "X" : "O");
+  }
   return (
     <>
       <header className="text-red-500">Jogo da Velha</header>
       <main>
-        <Board />
+        <p>{status}</p>
+        <Board xIsNext={xIsNext} squares={squares} onPlay={handlePlay} />
       </main>
     </>
   )
